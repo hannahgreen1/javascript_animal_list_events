8e9e337526baeee03e02c125cc2e8e97356900aa
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-22 11:51:26 +0100

    html done

---
+++ js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+
+    const handleFormSubmit = function(event){
+      event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const species = form.species.value;
+        const continent = form.continent.value;
+
+        const selectResult = document.querySelector('#select-result');
+        const readingList = document.querySelector('#reading-list');
+      readingList.textContent = ` ${title} ${author} ${category}`;
+
+      form.reset();
+    }
+
+  form.addEventListener('submit', handleFormSubmit);
+
+
+});
